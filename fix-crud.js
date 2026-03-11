c7dac5031d6429fef34c495bf9679f65d4daec35
Wayde <waydelyle@users.noreply.github.com>
2026-03-11 02:37:27 +0000

    chore: remove tracked scratch scripts

--- fix-crud.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/lib/server/session-tools/crud.ts', 'utf8');
-
-// The `preparedManagedSchedule` needs to be defined as `any` or `Schedule | null`
-content = content.replace(/let preparedManagedSchedule: Record<string, unknown> \| null = null/g, 'let preparedManagedSchedule: any = null');
-
-fs.writeFileSync('src/lib/server/session-tools/crud.ts', content);
--- split-agent-sheet-store.js
+++
@@ -1,1 +0,0 @@
-console.log('Skipping due to form complexity');
